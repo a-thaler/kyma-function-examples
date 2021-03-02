7507dfd6103be57a4466d93334fa3d01407a02d7
Thaler, Andreas <andreas.thaler01@sap.com>
2021-03-02 13:49:53 +0100

    init

--- telemetry/handler.js
+++ telemetry/handler.js
@@ -1,26 +1,45 @@
-const axios = require("axios");
+const axios = require("axios"); 
+const url = require("url");
+const wrapAxios = require('zipkin-instrumentation-axiosjs');
+const {HttpLogger} = require('zipkin-transport-http')
+const { Tracer, ExplicitContext, ConsoleRecorder,option: {Some, None}, Instrumentation,  BatchRecorder, jsonEncoder: {JSON_V2} } = require('zipkin');
+const recorder = new BatchRecorder({
+    logger: new HttpLogger({
+      endpoint: 'http://zipkin.kyma-system:9411/api/v2/spans',
+      jsonEncoder: JSON_V2
+    })
+  });
+const tracer = new Tracer({ ctxImpl:new ExplicitContext(), recorder: recorder, localServiceName: "svc-a" })
+const zipkinAxios = wrapAxios(axios, { tracer });
 
 module.exports = {
     main: async function (event, context) {
+      const req = event.extensions.request;
+
+      function readHeader(header) {
+        const val = req.header(header);
+        if (val != null) {
+          return new Some(val);
+        } else {
+          return None;
+        }
+      }
+console.log("Trace header: %s", JSON.stringify(req.headers));
+        const instrumentation = new Instrumentation.HttpServer({tracer,serviceName:"svc-a",port:0});
+        const id = instrumentation.recordRequest(req.method, url.parse(req.originalUrl), readHeader);
+        console.log("Trace Data: %s", JSON.stringify(id));
         console.log("Event data: %s", JSON.stringify(event.data));
-        var traceHeaders = extractTraceHeaders(event.extensions.request.headers)
 
-        let orderUrl = `${process.env.GATEWAY_URL}/test/orders/${encodeURIComponent(event.data.orderCode)}`
-        console.log("Order URL: %s", JSON.stringify(orderUrl));
-        let response = await axios.get(orderUrl, traceHeaders);
-        console.log("Order Data: %s", JSON.stringify(response.data));
+        /*if(Date.now() % 2 == 0){
+            res = event.extensions.response;
+            res.writeHead(400, {'Content-Type': 'text/plain', 'some': 'header'});
+            res.end();
+            console.log("Bad timing")
+            return
+        }*/
 
+        await zipkinAxios.post(`${process.env.SERVICE_URL}`, { orderCode: event.data.orderCode });
+        console.log(`message=svc-a notified, traceId=${id.traceId}`);
         return
     }
 };
-
-const traceHeaderNames = ['x-request-id', 'x-b3-traceid', 'x-b3-spanid', 'x-b3-parentspanid', 'x-b3-sampled', 'x-b3-Flags', 'x-ot-span-context']
-function extractTraceHeaders(headers) {
-    var map = {};
-    for (var i in traceHeaderNames) {
-        if (headers[traceHeaderNames[i]] !== undefined) {
-            map[traceHeaderNames[i]] = headers[traceHeaderNames[i]]
-        }
-    }
-    return { headers: map };
-}
